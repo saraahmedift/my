21a2decab226d31ea6dfbb98f9bab573abcc8241
saraahmedift <164875918+saraahmedift@users.noreply.github.com>
2024-03-25 17:57:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var myName = "Sarah";
+console.log(myName);
+name1 = "Sarah AHmed";
+console.log(name1);
+var myAge = "22";
+console.log(myAge);
+var myQualification = "graduation";
+console.log(myQualification);
+var myFavColor = "Black";
+console.log(myFavColor);
+myFavColor = "Grey";
+console.log(myFavColor);
+var myHobby = "reading";
+console.log(myHobby);
+myHobby = "gardening";
+console.log(myHobby);
